6d8950e3ac2a185e318552ed65018edbd1b10469
icewings <wings_of_wind@qq.com>
2017-03-09 17:16:55 +0800

    update FileObject
    update FileObject comment

--- JavaProject/FileObject.java
+++ JavaProject/FileObject.java
@@ -17,7 +17,7 @@ public class FileObject {
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		//Show browser for selecting file or directory
 		int returnVal = fileChooser.showOpenDialog(null);
-		if(returnVal == JFileChooser.APPROVE_OPTION){	//Selected successfully
+		if(returnVal == JFileChooser.APPROVE_OPTION){			//Selected successfully
 			//Get the path of selected file or directory
 			File selectedFile = fileChooser.getSelectedFile();
 			selectedFilePath = selectedFile.getPath();
@@ -32,23 +32,18 @@ public class FileObject {
 	 * @return Null
 	 */
 	public void deleteFiles(File deleteFiles) {
-		if (deleteFiles.isFile()) { // 若是文件，则直接删除
+		if (deleteFiles.isFile()) { 					//若是文件，则直接删除
 			deleteFiles.delete();
 			System.out.println(deleteFiles);
 		}
-		if (deleteFiles.isDirectory()) { 	// 若是目录
+		if (deleteFiles.isDirectory()) { 		//若是目录：包含2种目录，即空目录和非空目录
 			File[] childFile = deleteFiles.listFiles();
-			if(childFile.length > 0){			// 删除非空目录
-				for (int i = 0; i < childFile.length; i++) { // 遍历目录
-					if (childFile[i].isFile()) { 	// 若子目录是文件或空目录，则直接删除
-						childFile[i].delete();
-						System.out.println(childFile[i]);
-					} else { 							// 若是非空目录，则递归删除子文件
-						deleteFiles(childFile[i]);
-					}
-				} // for
+			if(childFile.length > 0){				//若是非空目录，则递归删除
+				for (int i = 0; i < childFile.length; i++) { 		// 遍历目录
+					deleteFiles(childFile[i]);		//递归删除子文件或子目录
+				} 
 			}			
-			if(deleteFiles.delete()){			// 删除空目录
+			if(deleteFiles.delete()){				//若是空目录，则直接删除
 				System.out.println(deleteFiles);
 			};
 		}
